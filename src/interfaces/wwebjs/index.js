b722602c6d46bd484f35363499cc54db1c6bd2e1
Danish Humair <me@danishhumair.com>
2024-04-03 09:15:47 +0500

    fixed wa-version issue

--- src/interfaces/wwebjs/index.js
+++ src/interfaces/wwebjs/index.js
@@ -4,10 +4,14 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
+    authStrategy: new LocalAuth(),
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
-    authStrategy: new LocalAuth()
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    }
 });
 
 client.on('ready', () => {
