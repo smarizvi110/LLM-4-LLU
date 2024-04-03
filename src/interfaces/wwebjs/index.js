92221bf369b994687f1c8ab12fcd1dfd30ff02de
Danish Humair <me@danishhumair.com>
2024-04-03 09:14:50 +0500

    made it headless 2 + localauth

--- src/interfaces/wwebjs/index.js
+++ src/interfaces/wwebjs/index.js
@@ -1,9 +1,14 @@
 console.log("LLM-4-LLU • WhatsApp Interface")
 
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+    authStrategy: new LocalAuth()
+});
 
 client.on('ready', () => {
     console.log('Client is ready!');
