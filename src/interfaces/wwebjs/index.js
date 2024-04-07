f6363ae5647b96a8c3ac11ace58624303880135b
Danish Humair <me@danishhumair.com>
2024-04-07 12:23:20 +0500

    refactoring

--- src/interfaces/wwebjs/index.js
+++ src/interfaces/wwebjs/index.js
@@ -12,20 +12,18 @@ console.log("\nCopyright (C) 2024");
 });
 console.log("\n");
 
-
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-    }
-});
+let clientConfig = { authStrategy: new LocalAuth() };
+clientConfig.puppeteer = { args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+// Hotfix for the latest WhatsApp Web version
+clientConfig.webVersionCache = {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+};
+
+const client = new Client(clientConfig);
 
 client.on('ready', () => {
     console.log('Client is ready!');
