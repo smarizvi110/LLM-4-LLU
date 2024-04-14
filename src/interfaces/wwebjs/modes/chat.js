e1658aa979be9129f7133a51d664ce3257e8e77a
Danish Humair <me@danishhumair.com>
2024-04-14 15:57:03 +0500

    added some boilerplate for modes

---
+++ src/interfaces/wwebjs/modes/chat.js
@@ -0,0 +1,11 @@
+const config = require("./config.json");
+const fs = require("fs");
+
+module.exports.run = async (msg, client, bot) => {
+    return;
+}
+
+module.exports.desc = {
+    name: "chat",
+    aliases: [],
+}
---
+++ src/interfaces/wwebjs/modes/ptt.js
@@ -0,0 +1,12 @@
+const config = require("./config.json");
+const fs = require("fs");
+const child_process = require('child_process');
+
+module.exports.run = async (msg, client, bot) => {
+    return;
+}
+
+module.exports.desc = {
+    name: "ptt",
+    aliases: [],
+}
